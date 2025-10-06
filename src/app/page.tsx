1706a54652b56a450e358ef6e04e978c110e9858
Mohd Rafi <dotnetexpertdev@gmail.com>
2025-10-06 15:49:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Portfolio from '../components/Portfolio';
-import ContactForm from '../components/ContactForm';
+import ContactForm from '../components/contact/ContactForm';
 import Careers from '@/components/Careers';
 
 export default function Home() {
